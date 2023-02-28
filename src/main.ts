cd699c4ad6582f26c44396e72012a253505aecee
til2to <padwusup@gmail.com>
2023-02-28 15:45:38 +0000

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,22 @@ import { ApolloServer } from 'apollo-server';
 import typeDefs from './schema';
 import resolvers from './resolvers';
 
+// const express = require('express');
+// const cors = require('cors');
+
+// const app = express();
+
+// app.use(cors());
+
 const server = new ApolloServer({
-    typeDefs: typeDefs,
-    resolvers: resolvers
+  typeDefs: typeDefs,
+  resolvers: resolvers,
+  cors: {
+    origin: '*', // Allow all origins
+    credentials: true, // Enable credentials
+  },
 });
 
 server.listen({ port: process.env.PORT || 3000 }).then(({ url }) => {
-    console.log(`🚀 Server ready at ${url}`);
+  console.log(`🚀 Server ready at ${url}`);
 });
